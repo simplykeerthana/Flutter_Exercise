2a07c87c657d2565724f4c72633b41ff016f45e1
Keerthana Madhavan <34112414+Madhakee01@users.noreply.github.com>
2019-12-22 14:17:41 -0500

    Update main.dart

--- 01/hello_world/lib/main.dart
+++ 01/hello_world/lib/main.dart
@@ -1,6 +1,6 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(app)
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
